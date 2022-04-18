afe1a2b462b2531c43870a9274bbcae62e269328
Tanu Roy <tanu.roy516@gmail.com>
2022-04-18 14:58:19 +0530

    init

---
+++ books.php
@@ -0,0 +1,51 @@
+<?php
+  session_start();
+  $count = 0;
+  // connecto database
+  require_once "./functions/database_functions.php";
+  $conn = db_connect();
+
+  $query = "SELECT book_isbn, book_image FROM books";
+  $result = mysqli_query($conn, $query);
+  if(!$result){
+    echo "Can't retrieve data " . mysqli_error($conn);
+    exit;
+  }
+
+  $title = "Catalogs of Books";
+  require_once "./template/header.php";
+?>
+  <link rel="stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="search.css"> 
+  <br>
+  <div class="search">
+  <form action="book_search.php" method = "POST">
+   <input type="text" placeholder=" Search....."  name="search"/>
+   <button>   
+   <i class="fa fa-search"  style="font-size: 18px;"> </i>
+   </button>   
+   </form>   
+   </div>
+   <br>   
+   <p class="lead text-center text-muted">Catalogs Of Books</p>     
+    <?php for($i = 0; $i < mysqli_num_rows($result); $i++){ ?>
+      <div class="row">
+        <?php while($query_row = mysqli_fetch_assoc($result)){ ?>
+          <div class="col-md-3">
+            <a href="book.php?bookisbn=<?php echo $query_row['book_isbn']; ?>">
+              <img class="img-responsive img-thumbnail" src="./bootstrap/img/<?php echo $query_row['book_image']; ?>">
+            </a>
+          </div>
+        <?php
+          $count++;
+          if($count >= 4){
+              $count = 0;
+              break;
+            }
+          } ?> 
+      </div>
+<?php
+      }
+  if(isset($conn)) { mysqli_close($conn); }
+  require_once "./template/footer.php";
+?>
---
+++ publisher_add.php
@@ -0,0 +1,54 @@
+<?php
+	session_start();
+	$title = "Add new publisher";
+	require "./template/header.php";
+	require "./functions/database_functions.php";
+	$conn = db_connect();
+
+	if(isset($_POST['add'])){
+		$pubid = trim($_POST['pubid']);
+		$pubid = mysqli_real_escape_string($conn, $pubid);
+		
+		$pubname = trim($_POST['pubname']);
+		$pubname = mysqli_real_escape_string($conn, $pubname);
+
+        $findPub = "SELECT * FROM publisher WHERE publisher_name = '$pubname'";
+		$findResult = mysqli_query($conn, $findPub);
+		if(!$findResult){
+			// insert into publisher table and return id
+			$insertPub = "INSERT INTO publisher(`publisherid`,`publisher_name`) VALUES ('$pubid','$pubname')";
+			$insertResult = mysqli_query($conn, $insertPub);
+			if(!$insertResult){
+				echo "Can't add new publisher " . mysqli_error($conn);
+				exit;
+			}
+        }
+        $query = "INSERT INTO publisher VALUES ('" . $pubid . "', '" . $pubname . "')";
+		$result = mysqli_query($conn, $query);
+		if(!$result){
+			echo "Can't add new data " . mysqli_error($conn);
+			exit;
+		} else {
+			header("Location: publisher_list.php");
+		}
+    }
+?>
+    <form method="post" action="publisher_add.php" enctype="multipart/form-data">
+		<table class="table">
+			<tr>
+				<th>Publisher Id</th>
+				<td><input type="text" name="pubid"></td>
+			</tr>
+			<tr>
+				<th>Publisher Name</th>
+				<td><input type="text" name="pubname" required></td>
+			</tr>
+        </table>
+		<input type="submit" name="add" value="Add new publisher" class="btn btn-primary">
+		<input type="reset" value="cancel" class="btn btn-default">
+	</form>
+	<br/>
+<?php
+	if(isset($conn)) {mysqli_close($conn);}
+	require_once "./template/footer.php";
+?>
